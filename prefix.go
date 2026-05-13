ca70eaba664a04bc208080fdf9615051f1dd890a
ugochi74 <confidenceodo4@gmail.com>
2026-05-13 16:08:50 +0000

    learning

---
+++ prefix.go
@@ -0,0 +1,21 @@
+package main
+
+import "strings"
+
+
+func longestCommonPrefix(strs []string) string {
+    if len(strs) == 0 {
+        return ""
+    }
+
+    prefix := strs[0]
+    for i := 1; i < len(strs); i++ {
+        for !strings.HasPrefix(strs[i], prefix) {
+            prefix = prefix[:len(prefix)-1]
+            if prefix == "" {
+                return ""
+            }
+        }
+    }
+    return prefix
+}
